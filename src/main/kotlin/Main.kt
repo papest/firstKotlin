6d6227a8486fb8fe9f8b0445481479198c15cd2c
Epap <peia@inbox.ru>
2023-01-10 14:20:20 +0300

    Color print

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
-
+const val ANSI_RESET = "\u001B[0m"
+const val ANSI_GREEN = "\u001B[32m"
     fun main() {
-        println("Hello, World!")
+        println(ANSI_GREEN + "Hello, World!" + ANSI_RESET)
     }
